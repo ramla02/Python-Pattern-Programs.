2b2e2634184129fee5f7672bacbb1a64173ac5c6
ramla02 <ramla252017@gmail.com>
2024-08-22 12:15:42 +0300

    pattern

--- 0-pattern1.py
+++ 0-pattern1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # this is a python program that prints asteriska pattern
 
-for x in range(1, 8):
-    for y in range(1, 20):
+for x in range(1, 11):
+    for y in range(1, 21):
         print("*", end="")
     print()
