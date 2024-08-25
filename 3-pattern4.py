5608a810b14f85b9fcc20d87e05bf794d07391f5
ramla02 <ramla252017@gmail.com>
2024-08-25 17:51:31 +0300

    pttr

---
+++ 3-pattern4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# python program pattern that prints 1 to 5 in descending order
+
+for x in range(5, 0, -1):
+    for y in range(5, 0, -1):
+        print(x, end="")
+    print()
