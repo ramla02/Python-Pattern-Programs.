84229b1fae179d30e40067548e7873e8800dc819
ramla02 <ramla252017@gmail.com>
2024-11-08 12:12:29 +0300

    pattern no 206

---
+++ pattern206.py
@@ -0,0 +1,10 @@
+n = 5
+
+for x in range(1, n + 1):
+    for y in range(1, n + 1):
+        if x <= n // 2:
+            print("0", end="  ")
+        else:
+            print("1", end="  ")
+    print()
+        
